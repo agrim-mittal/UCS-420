aca7ed4284f569a5db11789bc6de42e9ff933084
Agrim Mittal <agrimmittal@Agrims-MacBook-Air-2.local>
2026-08-04 09:01:44 +0530

    Added lab2.py

---
+++ lab2.py
@@ -0,0 +1,59 @@
+# c = int(input())
+# d = int(input())
+
+# if c > d:
+#     print("c is greater than d")
+# else:
+#     print("d is greater than c")
+# n = int(input("Enter a number: "))
+
+# if n <= 1:
+#     print("Not Prime")
+# else:
+#     is_prime = True
+
+#     for i in range(2, n):
+#         if n % i == 0:
+#             is_prime = False
+#             break
+
+#     if is_prime:
+#         print("Prime")
+#     else:
+#         print("Not Prime")
+
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+
+# largest = max(a, b, c)
+
+# print("Maximum number is:", largest)
+
+# n = int(input("Enter n: "))
+
+# sum = 0
+
+# for i in range(1, n + 1):
+#     if i % 7 == 0 and i % 9 == 0:
+#         sum += i
+
+# print("Sum =", sum)
+# n = int(input("Enter n: "))
+
+# sum = 0
+
+# for i in range(2, n + 1):
+#     prime = True
+
+#     for j in range(2, int(i ** 0.5) + 1):
+#         if i % j == 0:
+#             prime = False
+#             break
+
+#     if prime:
+#         sum += i
+
+# print("Sum of prime numbers =", sum)
+
